3efc15cd4dbc76f36da4236b11444b940de11642
Mohamed Sayed <creative.mohamed.sayed@gmail.com>
2021-09-28 11:35:46 +0200

    code + docs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,49 @@
-## 0.0.1
+# Logs
 
-* TODO: Describe initial release.
+## 3.0.3
+
+* change template to dart package
+
+## 3.0.2
+
+* added level parameter
+
+## 3.0.1
+
+* Re-write package
+* Fixed - not working in some environments
+* Removed asni package
+
+## 2.2.4 | 2.2.5
+
+* handle big text
+
+## 2.1.2+7 | 2.1.3 | 2.1.4
+
+* auto toString()
+
+## 2.0.2+6
+
+* docs
+
+## 2.0.1+6
+
+* docs
+
+## 2.0.0+6
+
+* added `gray()`
+
+## 1.0.1+5
+
+* Enhanced Documentation
+* Fixed Tiny Bug
+* Fixed iOS Bug
+
+## 1.0.0
+
+* Now it's ready
+
+## 0.1.0
+
+* Beta Version
--- lib/print_color.dart
+++ lib/print_color.dart
@@ -1,7 +1,53 @@
 library print_color;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:developer';
+
+///------------------------------------
+/// Print.color()
+///------------------------------------
+class Print {
+  ///------------------------------------
+  /// Print to console in black
+  ///------------------------------------
+  static black(text, {name = 'black'}) =>
+      log('\x1B[30m$text\x1B[0m', name: name);
+
+  ///------------------------------------
+  /// Print to console in red
+  ///------------------------------------
+  static red(text, {name = 'red'}) => log('\x1B[31m$text\x1B[0m', name: name);
+
+  ///------------------------------------
+  /// Print to console in green
+  ///------------------------------------
+  static green(text, {name = 'green'}) =>
+      log('\x1B[32m$text\x1B[0m', name: name);
+
+  ///------------------------------------
+  /// Print to console in yellow
+  ///------------------------------------
+  static yellow(text, {name = 'yellow'}) =>
+      log('\x1B[33m$text\x1B[0m', name: name);
+
+  ///------------------------------------
+  /// Print to console in blue
+  ///------------------------------------
+  static blue(text, {name = 'blue'}) => log('\x1B[34m$text\x1B[0m', name: name);
+
+  ///------------------------------------
+  /// Print to console in magenta
+  ///------------------------------------
+  static magenta(text, {name = 'magenta'}) =>
+      log('\x1B[35m$text\x1B[0m', name: name);
+
+  ///------------------------------------
+  /// Print to console in cyan
+  ///------------------------------------
+  static cyan(text, {name = 'cyan'}) => log('\x1B[36m$text\x1B[0m', name: name);
+
+  ///------------------------------------
+  /// Print to console in white
+  ///------------------------------------
+  static white(text, {name = 'white'}) =>
+      log('\x1B[37m$text\x1B[0m', name: name);
 }
